dc002d449086a9dfcf9d64bf6eabfb2a87dc587a
Filip <filip5871@gmail.com>
2019-10-17 01:58:36 +0200

    Sorry Kaito, my mistake :(

--- src/main/kotlin/channels/kaito/Commands.kt
+++ src/main/kotlin/channels/kaito/Commands.kt
@@ -22,13 +22,12 @@ fun TwitchScope.kaitoCommands() {
             sendMessage("You can call me using following commands: \"hug, clap, uno, sad, gn, food, treat \" CoolStoryBob")
         }
 
-        "treat {treat}" receive { parameters ->
-            when(val treat = parameters.getValue("treat")) {
+        "treat {value}" receive { parameters ->
+            when(val treat = parameters.getValue("value")) {
                 "cat" -> sendMessage("The Cat shall get a Treat! \uD83D\uDC40")
                 "dog" -> sendMessage("The Dogs Shall get a Treat! \uD83D\uDC40")
                 else -> sendMessage("No I don't have a $treat \uD83D\uDC40")
             }
-
         }
     }
 }
